dbd7ec259bcfd463ee994c797c2179cb53e9c16e
harshraj93 <57796103+harshraj93@users.noreply.github.com>
2020-07-22 16:33:17 +0530

    Updates

---
+++ precache-manifest.76fe0e7aa3ad5a43d73d678f9e42a8c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d688bbb9f723a3f88589d5b2b62413b",
+    "url": "/smart-assistant/index.html"
+  },
+  {
+    "revision": "a7dc6ec980500e40b94e",
+    "url": "/smart-assistant/static/css/2.44a8782a.chunk.css"
+  },
+  {
+    "revision": "46355dab46cd79316031",
+    "url": "/smart-assistant/static/css/main.37e2a18c.chunk.css"
+  },
+  {
+    "revision": "a7dc6ec980500e40b94e",
+    "url": "/smart-assistant/static/js/2.08b7318a.chunk.js"
+  },
+  {
+    "revision": "389dcbdaeb668cb2ae96d13324da45bc",
+    "url": "/smart-assistant/static/js/2.08b7318a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46355dab46cd79316031",
+    "url": "/smart-assistant/static/js/main.112a2281.chunk.js"
+  },
+  {
+    "revision": "b961366f272ecfe0be09",
+    "url": "/smart-assistant/static/js/runtime-main.e022ae6c.js"
+  },
+  {
+    "revision": "5132d40a8af5aa5c5257f2984011f586",
+    "url": "/smart-assistant/static/media/Deloitte_logo.5132d40a.png"
+  },
+  {
+    "revision": "85103179d85575a6c938adbf6a02d9ee",
+    "url": "/smart-assistant/static/media/back-arrow.85103179.svg"
+  },
+  {
+    "revision": "c5ec275692df9ff3e3a5f4f60f6a13b3",
+    "url": "/smart-assistant/static/media/zeni.c5ec2756.png"
+  }
+]);
